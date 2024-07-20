bcf2619291e1aeaeeafd9f58bfab81adf8790e23
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-20 18:12:58 +0200

    feat: :sparkles: create a new Giphy api key and hide it into the .env file

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,14 @@
-export const getGifs = async( category ) => {
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${
+    import.meta.env.VITE_GIPHY_API
+  }&q=${category}&limit=10`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
-    const resp = await fetch( url );
-    const { data } = await resp.json();
-
-    const gifs = data.map( img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }));
-    
-    return gifs;
-}
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
