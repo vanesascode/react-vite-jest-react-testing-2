8d3c42adec77bd21d67bc7c7700702ffe603d1ea
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-20 18:10:14 +0200

    refactor: :recycle: format of AddCategory component with prettier

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
 
-    const [ inputValue, setInputValue ] = useState('');
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
 
-    const onInputChange = ({ target }) => {
-        setInputValue( target.value );
-    }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setInputValue("");
+    // Sending the input value to the parent component:
+    onNewCategory(inputValue.trim());
+  };
 
-    const onSubmit = ( event ) => {
-        event.preventDefault();
-        if( inputValue.trim().length <= 1) return;
-
-        // setCategories( categories => [ inputValue, ...categories ]);
-        setInputValue('');
-        onNewCategory( inputValue.trim() );
-    }
-
-    return (
-        <form onSubmit={ onSubmit }>
-            <input 
-                type="text"
-                placeholder="Buscar gifs"
-                value={ inputValue }
-                onChange={ onInputChange }
-            />
-        </form>
-    )
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
