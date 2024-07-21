0205582b870d2202c67abcf989ead68b857a8ed6
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-21 16:27:04 +0200

    refactor: :recycle: format the useFetchGifs custom hook

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,26 +1,22 @@
-import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
-export const useFetchGifs = ( category ) => {
- 
-    const [images, setImages] = useState([]);
-    const [isLoading, setIsLoading] = useState( true );
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const getImages = async() => {
-        const newImages = await getGifs( category );
-        setImages(newImages);
-        setIsLoading(false);
-    }
-    
-    useEffect( () => {
-        getImages();
-    }, []);
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
 
+  useEffect(() => {
+    getImages();
+  }, []);
 
-
-    return {
-        images,
-        isLoading
-    }
-
-}
+  return {
+    images,
+    isLoading,
+  };
+};
