669379c916b45c04ac6af2b9378ecc7286804fce
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-21 16:24:47 +0200

    feat: :sparkles: add the propTypes in the GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,30 +1,24 @@
-import { GifItem } from './GifItem';
-import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import PropTypes from "prop-types";
 
 export const GifGrid = ({ category }) => {
+  const { images, isLoading } = useFetchGifs(category);
 
-    const { images, isLoading } = useFetchGifs( category );
-    
-    return (
-        <>
-            <h3>{ category }</h3>
-            {
-                isLoading && ( <h2>Cargando...</h2> )
-            }
-            
+  return (
+    <>
+      <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+      <div className="card-grid" data-testid="grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
+      u
+    </>
+  );
+};
 
-            <div className="card-grid">
-                {
-                    images.map( ( image ) => (
-                        <GifItem 
-                            key={ image.id } 
-                            { ...image }
-                        />
-                    ))
-                }
-                
-            </div>
-
-        </>
-    )
-}
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+};
