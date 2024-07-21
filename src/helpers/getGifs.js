86d1536e4a187d71de876002e0e48233bd2ff3d5
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-21 16:26:38 +0200

    refactor: :recycle: add the api key directly to the getGifs function, so Jest tests work

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,9 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${
-    import.meta.env.VITE_GIPHY_API
-  }&q=${category}&limit=10`;
+  // const url = `https://api.giphy.com/v1/gifs/search?api_key=${
+  //   import.meta.env.VITE_GIPHY_API
+  // }&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=Z6QCElSWBMQrPpc7H8Fxbn0jJXPDsNS2&q=${category}&limit=10`;
+
   const resp = await fetch(url);
   const { data } = await resp.json();
 
